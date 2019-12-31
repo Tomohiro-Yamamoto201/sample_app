6a201b8323306e817d2c888059a601d67381c17f
Tomohiro <tomohiro.y201@gmail.com>
2019-12-31 12:46:41 +0000

    mail-preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,11 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'd74ef6157f35408a8731d4e3237fcf1d.vfs.cloud9.ap-northeast-1.amazonaws.com'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
 
   config.action_mailer.perform_caching = false
 
