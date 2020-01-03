44da26d5e65e9d3bb2e2baaba0400f2adc1a0276
Tomohiro <tomohiro.y201@gmail.com>
2020-01-03 04:30:58 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://intense-mesa-09796.herokuapp.com/'
+  host = 'https://intense-mesa-09796.herokuapp.com/password_resets/edit'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
