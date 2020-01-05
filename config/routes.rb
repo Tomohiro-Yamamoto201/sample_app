bdb83014a472a3fc40ae765e00a973c74e0c78d4
Tomohiro <tomohiro.y201@gmail.com>
2020-01-05 01:46:13 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  get '/account_activations',  to: 'account_activations#edit'
   
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
